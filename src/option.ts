b23473e1eca72853d92b41d64e4f9acd8ba86f20
Alice Pote <alice.writes.wrongs@gmail.com>
2022-04-08 09:19:40 -0400

    I was doing something here...

--- src/option.ts
+++ src/option.ts
@@ -2,20 +2,20 @@
  * Abstract class for defining the methods shared between Some
  * and None.
  */
-abstract class AbstractOption<T> {
+interface AbstractOption<T> {
   /**
-   * Is it Some what you need or is it None?
+   * Is it Some of what you need or is it None?
    *
    * returns true if this is a Some, else false
    */
-  abstract isSome(): this is Some<T>
+  isSome(): this is Some<T>
 
   /**
    * Is it None of what you need, or is it Some-thing?
    *
    * return true if this is a None, else false
    */
-  abstract isNone(): this is None<T>
+  isNone(): this is None
 
   /**
    * Map across the value wrapped up in the Option
@@ -23,75 +23,151 @@ abstract class AbstractOption<T> {
    * If Some(x), returns Some(fn(x)), if None, returns
    * None.
    */
-  abstract map<S>(fn: (value: T) => S): Option<S>
+  map<S>(fn: (value: T) => S): Option<S>
 
   /**
    * Filter the Option, reducing Some(x) to None if
    * predicate(x) !== true.
    */
-  abstract filter(predicate: (value: T) => boolean): Option<T>
+  filter(predicate: (value: T) => boolean): Option<T>
 
   /**
    * Unwrap the value in the Maybe. If Some(x), returns x, if
    * None returns defaultVal.
    */
-  abstract unwrap(defaultVal: T): T
+  unwrap(defaultVal: T): T
 }
 
-export class Some<T> extends AbstractOption<T> {
+interface Some<T> extends AbstractOption<T> {
   value: T
+  isSome: () => true
+  isNone: () => false
+  map<S>(fn: (value: T) => S): Some<S>
+  filter: (func: (value: T) => boolean) => Option<T>
+  unwrap: (defaultVal: T) => T
+}
 
-  constructor(value: T) {
-    super()
-    this.value = value
-  }
+export const Some = <T>(val: T): Some<T> => {
+  let some: Some<T> = {
+    value: val,
+    isSome() {
+      return true
+    },
+    isNone() {
+      return false
+    },
+    map<S>(fn: (value: T) => S): Some<S> {
+      return Some(fn(this.value))
+  },
 
-  isSome() {
-    return true
-  }
-
-  isNone() {
-    return false
-  }
-
-  map<S>(fn: (value: T) => S): Some<S> {
-    return new Some(fn(this.value))
-  }
-
-  filter(predicate: (value: T) => boolean): Some<T> | None<T> {
+  filter(predicate: (value: T) => boolean): Option<T> {
     if (predicate(this.value)) {
       return this
     } else {
-      return new None<T>()
+      return None
     }
-  }
-
+  },
   unwrap(_defaultVal: T): T {
     return this.value
   }
+  }
+
+  return some
 }
 
-export class None<T> extends AbstractOption<T> {
+interface None extends AbstractOption<any> {
+  isSome: () => false
+  isNone: () => true
+  map: <S>(fn: (value: any) => S) => None
+  filter: (_predicate: (value: any) => boolean) => None
+  unwrap: <T>(defaultVal: T) => T
+}
+
+
+const None: None = {
   isSome() {
     return false
-  }
-
+  },
   isNone() {
     return true
-  }
-
-  map<S>(_fn: (value: T) => S): None<S> {
-    return new None<S>()
-  }
-
-  filter(_predicate: (value: T) => boolean): None<T> {
+  },
+  map<S>(_fn: (value: any) => S): None {
     return this
-  }
-
-  unwrap(defaultVal: T): T {
+  },
+  filter<T>(_predicate: (value: T) => boolean): None {
+    return this
+  },
+  unwrap<T>(defaultVal: T): T {
     return defaultVal
   }
 }
+Object.freeze(None)
+
+
+
+
+//   unwrap(_defaultVal: T): T {
+//     return this.value
+//   }
+// }
+
+
+
+
+// export class Some<T> extends AbstractOption<T> {
+//   value: T
+
+//   constructor(value: T) {
+//     super()
+//     this.value = value
+//   }
+
+//   isSome() {
+//     return true
+//   }
+
+//   isNone() {
+//     return false
+//   }
+
+//   map<S>(fn: (value: T) => S): Some<S> {
+//     return new Some(fn(this.value))
+//   }
+
+//   filter(predicate: (value: T) => boolean): Some<T> | None<T> {
+//     if (predicate(this.value)) {
+//       return this
+//     } else {
+//       return new None<T>()
+//     }
+//   }
+
+//   unwrap(_defaultVal: T): T {
+//     return this.value
+//   }
+// }
+
+// export class None<T> extends AbstractOption<T> {
+//   isSome() {
+//     return false
+//   }
+
+//   isNone() {
+//     return true
+//   }
+
+//   map<S>(_fn: (value: T) => S): None<S> {
+//     return new None<S>()
+//   }
+
+//   filter(_predicate: (value: T) => boolean): None<T> {
+//     return this
+//   }
+
+//   unwrap(defaultVal: T): T {
+//     return defaultVal
+//   }
+// }
 
 /**
  * An Option type which allows a nice encapsulation of optional values.
@@ -104,7 +180,7 @@ export class None<T> extends AbstractOption<T> {
  * const optionalNumber = new Some(13)
  * ```
  */
-export type Option<T> = Some<T> | None<T>
+export type Option<T> = Some<T> | None
 
 /**
  * Turn a 'possibly-null' value (i.e. `string | null`) into a proper
@@ -115,8 +191,8 @@ export type Option<T> = Some<T> | None<T>
  */
 export function toOption<T>(val: T | undefined | null): Option<T> {
   if (val === undefined || val === null) {
-    return new None()
+    return None
   } else {
-    return new Some(val)
+    return Some(val)
   }
 }
